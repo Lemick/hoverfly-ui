e6cf5b7205df5120da2170db3ee5f8c7c7f79ded
Mickaël Beguin <sidfloyd84@gmail.com>
2023-09-24 17:22:35 +0200

    Update build plugin workflow

---
+++ intellij-plugin/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## [Unreleased]
+
+[Unreleased]: https://github.com/Lemick/hoverfly-ui/commits
+
+## [0.0.1]
+
+### Added
+
+- Initial project
--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -1,27 +1,45 @@
+import org.jetbrains.changelog.Changelog
+
+fun Project.localGradleProperty(name: String): Provider<String> = provider {
+    if (hasProperty(name)) property(name)?.toString() else null
+}
+
+fun environment(key: String) = project.providers.environmentVariable(key)
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.changelog") version "2.2.0"
 }
 
-group = "com.github.lemick"
-version = "1.0-SNAPSHOT"
+val pluginVersion: String = localGradleProperty("pluginVersion").get()
+
+group = localGradleProperty("pluginGroup").get()
+version = pluginVersion
 
 repositories {
     mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
-    type.set("IC") // Target IDE Platform
+    pluginName = localGradleProperty("pluginName")
+    version = localGradleProperty("platformVersion")
+    type = localGradleProperty("platformType")
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins = localGradleProperty("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
 }
 
 val copyUi = tasks.register("copyUi", Copy::class) {
     from("$rootDir/ui/build")
     into("$rootDir/intellij-plugin/build/resources/main/hoverfly-ui")
     dependsOn("::ui:build")
+}
+
+changelog {
+    groups.empty()
+    repositoryUrl = localGradleProperty("pluginRepositoryUrl")
 }
 
 tasks.jar {
@@ -33,11 +51,29 @@ tasks.instrumentedJar {
 }
 
 tasks {
+    val changelog = project.changelog // local variable for configuration cache compatibility
+
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    // Get the latest available change notes from the changelog file
+    with(changelog) {
+        renderItem(
+            (getOrNull(version.get()) ?: getUnreleased())
+                .withHeader(false)
+                .withEmptySections(false),
+            Changelog.OutputType.HTML,
+        )
+    }
+
+
     // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
+
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
@@ -54,6 +90,12 @@ tasks {
     }
 
     publishPlugin {
+        dependsOn("patchChangelog")
         token.set(System.getenv("PUBLISH_TOKEN"))
+
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels = listOf(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
     }
 }
