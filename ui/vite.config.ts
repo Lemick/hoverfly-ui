a640bb44668a6fdf9d66b9c639fdd1234440130e
Mickaël Beguin <sidfloyd84@gmail.com>
2023-10-21 16:53:09 +0200

    [CHORE] Fix public path

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,21 +1,24 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/',
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        css: true,
-        include: ["src/**/*.test.*"],
-        reporters: ['verbose'],
-        coverage: {
-            reporter: ['text', 'json', 'html'],
-            include: ['src/**/*'],
-            exclude: [],
-        }
+  base: '/hoverfly-ui',
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    include: ['src/**/*.test.*'],
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: []
     }
+  }
 });
