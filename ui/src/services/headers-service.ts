2f3931a3fddbd587f2a5a68d47fc78fa8995ae48
Mickaël Beguin <sidfloyd84@gmail.com>
2024-02-18 13:23:50 +0100

    [CHORE] Test preview

--- ui/src/services/headers-service.ts
+++ ui/src/services/headers-service.ts
@@ -2,6 +2,7 @@ import { ResponseHeaders } from '../types/hoverfly';
 
 export const byteLengthUtf8 = (str: string) => new Blob([str]).size;
 
+// TODO remove
 export const updateContentLengthAccordingToBody = (
   body: string,
   headers: ResponseHeaders | undefined
