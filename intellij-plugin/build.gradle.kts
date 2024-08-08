51b390f3ab16acd63b794dc73ea5f03aef087981
Clement Stoquart <clemstoquart@gmail.com>
2024-08-08 23:58:19 +0200

    [CHORE] Intellij 2024.2 compatibility

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -6,8 +6,8 @@ fun environment(key: String) = project.providers.environmentVariable(key)
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.23"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
 }
 
 val pluginVersion: String = localGradleProperty("pluginVersion").get()
