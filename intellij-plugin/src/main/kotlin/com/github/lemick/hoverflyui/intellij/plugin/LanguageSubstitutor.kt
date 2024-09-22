ab6d3f2b9c317be5cf4682a897e22099a8d67fb2
Mickaël Beguin <sidfloyd84@gmail.com>
2024-09-22 23:15:37 +0200

    [BUGFIX] Fix language editor

--- intellij-plugin/src/main/kotlin/com/github/lemick/hoverflyui/intellij/plugin/LanguageSubstitutor.kt
+++ intellij-plugin/src/main/kotlin/com/github/lemick/hoverflyui/intellij/plugin/LanguageSubstitutor.kt
@@ -1,7 +1,7 @@
 package com.github.lemick.hoverflyui.intellij.plugin
 
-import com.intellij.json.JsonLanguage
 import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.LanguageSubstitutor
@@ -10,7 +10,7 @@ class LanguageSubstitutor : LanguageSubstitutor() {
     override fun getLanguage(file: VirtualFile, project: Project): Language? {
         val name = file.name
         if (name.endsWith(SimulationsFileType.INSTANCE.defaultExtension)) {
-            return JsonLanguage.INSTANCE
+            return PlainTextLanguage.INSTANCE // JSON is a better choice, but has been extracted from 2024.3, need to find a way to use it with retro compatibility
         }
         return null
     }
