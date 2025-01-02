69f4558a1ad46a680c4b29d3399dc0ee2ec3e52c
Mickaël Beguin <sidfloyd84@gmail.com>
2025-01-02 18:12:55 +0100

    [CHORE] Bump versions

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,9 +5,22 @@ import path from 'path';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   build: {
     outDir: 'build'
+  },
+  output: {
+    manualChunks(id: string) {
+      if (id.indexOf('node_modules') !== -1) {
+        const basic = id.toString().split('node_modules/')[1];
+        const sub1 = basic.split('/')[0];
+        if (sub1 !== '.pnpm') {
+          return sub1.toString();
+        }
+        const name2 = basic.split('/')[1];
+        return name2.split('@')[name2[0] === '@' ? 1 : 0].toString();
+      }
+    }
   },
   resolve: {
     alias: {
