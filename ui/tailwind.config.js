dc691e5ac5e2bcb18b1454a6c9abe35cd4be8ec9
Mickaël Beguin <sidfloyd84@gmail.com>
2025-07-27 13:30:13 +0200

    [CHORE] Continue migration

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,5 +1,3 @@
-import animate from 'tailwindcss-animate';
-
 const config = {
   darkMode: 'class',
   content: [
@@ -20,45 +18,10 @@ const config = {
       fontFamily: {
         sans: ['Inter var', 'Segoe UI', 'sans-serif'],
       },
-      colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
-        },
-        destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
-        },
-        muted: {
-          DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)',
-        },
-        accent: {
-          DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)',
-        },
-        popover: {
-          DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
-        },
-        card: {
-          DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
-      },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: 'calc(var(--radius) + 2px)',
+        md: 'var(--radius)',
+        sm: 'calc(var(--radius) - 2px)',
       },
       keyframes: {
         'accordion-down': {
@@ -76,7 +39,6 @@ const config = {
       },
     },
   },
-  plugins: [animate],
 };
 
 export default config;
